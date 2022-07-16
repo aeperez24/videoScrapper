0f3d3ef304f0846bba05c73a2fe1349ebc6ce596
andres perez <laandres.perez@andress-MacBook-Pro.local>
2022-07-16 13:35:50 -0400

    adding tests for cuevana downloader

--- provider/cuevana/downloaderService_test.go
+++ provider/cuevana/downloaderService_test.go
@@ -2,19 +2,42 @@ package cuevana
 
 import (
 	"aeperez24/animewatcher/service"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var ds = DownloaderService{ScrapService: ScrapperService{}, HttpWrapper: buildHttpWrapperMock(),
+	getProxies: mockGetProxies, getClientWithProxies: mockGetClientWithProxy, usedProxies: make(map[string]bool)}
+
 func TestMustReturnSortedEpisodesLinks(t *testing.T) {
-	httpWrapper := buildHttpWrapperMock()
-	ds := DownloaderService{ScrapService: ScrapperService{}, HttpWrapper: httpWrapper}
 	links, _ := ds.GetSortedEpisodesLinks("serieLink")
 	assert.Equal(t, 30, len(links))
-	assert.True(t, contains(links, "https://ww3.cuevana3.me/episodio/servant-3x9"))
+	assert.True(t, contains(links, "https://ww3.cuevana3.me/episodio/servant-1x1"))
+}
+
+func TestMustReturnEpisodesNames(t *testing.T) {
+	episodes, _ := ds.GetSortedEpisodesAvaliable("serieLink")
+	assert.Equal(t, 30, len(episodes))
+
+	fmt.Println(episodes)
+	assert.True(t, contains(episodes, "servant-1x1"))
+}
+
+func TestMustReturnDownloadEpisodeFromLink(t *testing.T) {
+	video, format, err := ds.DownloadEpisodeFromLink("serieLink", "servant-1x1")
+	assert.Nil(t, err)
+	bytes, _ := ioutil.ReadAll(video)
+	assert.Equal(t, "video", string(bytes))
+	assert.Equal(t, "mp4", format)
+
 }
 
 func buildHttpWrapperMock() service.HttpWrapper {
@@ -23,6 +46,25 @@ func buildHttpWrapperMock() service.HttpWrapper {
 	httpWrapper.On("Get", "serieLink").Return(&http.Response{
 		Body: open("inputs/seriepage.html"),
 	}, nil)
+
+	httpWrapper.On("Get", "https://ww3.cuevana3.me/episodio/servant-1x1").Return(&http.Response{
+		Body: open("inputs/episodepage.html"),
+	}, nil)
+
+	httpWrapper.On("Get", "https://1fichier.com/?o0oflhfdfby481t7e3bg#Synchronization+Service").Return(&http.Response{
+		Body: open("inputs/1fichier.html"),
+	}, nil)
+
+	httpWrapper.On("PostForm", "https://1fichier.com/?o0oflhfdfby481t7e3bg",
+		url.Values{"adz": []string{"313.776397987367"}, "dl_no_ssl": []string{"on"}, "dlinline": []string{"on"}}).
+		Return(&http.Response{
+			Body: open("inputs/downloadpage.html"),
+		}, nil)
+
+	httpWrapper.On("Get", "http://a-11.1fichier.com/c262664672?inline").
+		Return(&http.Response{
+			Body: io.NopCloser(strings.NewReader("video")),
+		}, nil)
 
 	return httpWrapper
 }
@@ -40,4 +82,12 @@ func contains(s []string, str string) bool {
 	}
 
 	return false
+}
+
+func mockGetProxies() []string {
+	return []string{"proxy"}
+}
+
+func mockGetClientWithProxy(in []string) (httpPostClient, string) {
+	return buildHttpWrapperMock(), in[0]
 }
