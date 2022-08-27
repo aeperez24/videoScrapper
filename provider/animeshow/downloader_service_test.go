b186bbf4f08be5495d2cdffab94663b414c80c36
andres perez <laandres.perez@andress-MacBook-Pro.local>
2022-08-27 16:15:41 -0400

    improving coverage

--- provider/animeshow/downloader_service_test.go
+++ provider/animeshow/downloader_service_test.go
@@ -3,16 +3,25 @@ package animeshow
 import (
 	serviceMock "aeperez24/animewatcher/mock/service"
 	"aeperez24/animewatcher/service"
-	"log"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
-const SERIE_LINK = "serieLink"
+const EPISODE_LINK = "https://www2.animeshow.tv/Fullmetal-Alchemist-Brotherhood-episode-1/"
+const SERIE_LINK = "https://www2.animeshow.tv/Fullmetal-Alchemist-Brotherhood/"
+
+const DOWNLOAD_URL = "https://www.mp4upload.com/a6xkfdysqdbu"
+const LOCATION_URL = "location"
+const VIDEO_DOWNLOAD = "videoDownload"
+const MIRROR_LINK = "https://www2.animeshow.tv/Tate-no-Yuusha-no-Nariagari-Season-2-episode-2-mirror-3/"
 
 func TestGetEpisodesAvaliable(t *testing.T) {
 	ds := DowloaderService{
@@ -20,12 +29,6 @@ func TestGetEpisodesAvaliable(t *testing.T) {
 		HttpWrapper:      buildHttpWrapperMock(),
 		AppConfiguration: service.AppConfiguration{},
 	}
-	htmlFile, err := os.Open("inputs/episodesList.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer htmlFile.Close()
-
 	episodesList, _ := ds.getEpisodesAvaliable(SERIE_LINK)
 	assert.Len(t, episodesList, 64, "the size expected is 64")
 	assert.Equal(t, "https://www2.animeshow.tv/Fullmetal-Alchemist-Brotherhood-episode-1/", episodesList[0], "")
@@ -38,15 +41,32 @@ func TestGetSortedEpisodesAvaliable(t *testing.T) {
 		HttpWrapper:      buildHttpWrapperMock(),
 		AppConfiguration: service.AppConfiguration{},
 	}
-	htmlFile, err := os.Open("inputs/episodesList.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer htmlFile.Close()
-
 	episodesList, _ := ds.GetSortedEpisodesAvaliable(SERIE_LINK)
 	assert.Len(t, episodesList, 64, "the size expected is 64")
 	assert.Equal(t, "1", episodesList[0], "")
+}
+
+func TestDownloadFromM4upload(t *testing.T) {
+	ds := DowloaderService{
+		ScrapService:     ScrapperServiceImpl{},
+		HttpWrapper:      buildHttpWrapperMock(),
+		AppConfiguration: service.AppConfiguration{},
+	}
+
+	reader, _, _ := ds.downloadFromM4upload(DOWNLOAD_URL)
+	result, _ := ioutil.ReadAll(reader)
+	assert.Equal(t, VIDEO_DOWNLOAD, string(result))
+}
+
+func TestDownloadFromLink(t *testing.T) {
+	ds := DowloaderService{
+		ScrapService:     ScrapperServiceImpl{},
+		HttpWrapper:      buildHttpWrapperMock(),
+		AppConfiguration: service.AppConfiguration{},
+	}
+	reader, _, _ := ds.DownloadEpisodeFromLink(SERIE_LINK, "1")
+	result, _ := ioutil.ReadAll(reader)
+	assert.Equal(t, VIDEO_DOWNLOAD, string(result))
 }
 
 func buildHttpWrapperMock() service.HttpWrapper {
@@ -54,6 +74,28 @@ func buildHttpWrapperMock() service.HttpWrapper {
 	httpWrapper.On("Get", SERIE_LINK).Return(&http.Response{
 		Body: open("inputs/episodesList.html"),
 	}, nil)
+
+	httpWrapper.On("Get", EPISODE_LINK).Return(&http.Response{
+		Body: open("inputs/episode.html"),
+	}, nil)
+
+	httpWrapper.On("Get", MIRROR_LINK).Return(&http.Response{
+		Body: open("inputs/episodeMU.html"),
+	}, nil)
+
+	headersLocation := http.Header{}
+	headersLocation["Location"] = []string{LOCATION_URL}
+	httpWrapper.On("Request", DOWNLOAD_URL, "POST", mock.Anything).Return(&http.Response{
+		Header: headersLocation,
+		Body:   io.NopCloser(strings.NewReader("")),
+	}, nil)
+
+	headersRefer := make(map[string]string)
+	headersRefer["Referer"] = "https://www.mp4upload.com/"
+	httpWrapper.On("RequestWithHeaders", LOCATION_URL, "GET", nil, headersRefer).Return(&http.Response{
+		Body: io.NopCloser(strings.NewReader(VIDEO_DOWNLOAD)),
+	}, nil)
+
 	return &httpWrapper
 }
 
